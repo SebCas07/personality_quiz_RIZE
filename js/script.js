5689ce08c957c37f8f31f97b95dd5ed89546f593
SebCas07 <tiancas07@gmail.com>
2026-03-22 23:22:13 -0500

    Added scoring map, about to start description shown with each result

--- js/script.js
+++ js/script.js
@@ -21,3 +21,29 @@ const questionBlocks = document.querySelectorAll(".question-block");
         });
     });
  });
+
+// SCoring map 
+
+const scoringMap = [
+    
+    // Question 1
+    { A: "Hip-Hop", B: "Pop", C: "Rock", D: "R&B"}, 
+
+    // Question 2
+    { A: "Hip-Hop", B: "Pop", C: "Latin", D: "Electronic (EDM)"},
+
+    // Question 3
+    { A: "R&B", B: "Jazz", C: "CLassical", D: "Indie"},
+
+    // Question 4
+    { A: "Country", B: "Metal", C: "Classical", D: "R&Electronic (EDM)"},
+
+    // Question 5
+    { A: "Afrobeats", B: "Indie", C: "Rock", D: "K-Pop"}
+]
+
+// descriptions shown with each result 
+
+const genreDescriptions = { 
+    
+}
