7f95032ea6b89a69831fc729bfbf7640de822a0b
SebCas07 <tiancas07@gmail.com>
2026-03-22 23:12:37 -0500

    added JS function so that when a choice is selected it is now highlighted

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 console.log("script.js connected!");
+
+const selectedAnswers = {}; 
+
+// toggle button fill and trsack answer 
+const questionBlocks = document.querySelectorAll(".question-block"); 
+ questionBlocks.forEach((block, questionIndex) => { 
+    const buttons = block.querySelectorAll(".answer-btn"); 
+
+    buttons.forEach((btn) => { 
+        btn.addEventListener("click", () => { 
+            // remove selected from every button in this block
+            buttons.forEach((b) => b.classList.remove("selected")); 
+
+            // highligjt the clicked button
+            btn.classList.add("selected"); 
+
+            // stores the answer letter for the question 
+            selectedAnswers[questionIndex] = btn.getAttribute("data-answer"); 
+
+        });
+    });
+ });
